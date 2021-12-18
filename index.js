8ffee7eec4d0044490705664039c2d9742ae8544
togo-mentor <uint2435@gmail.com>
2021-12-18 17:30:09 +0900

    test

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ var app = express();
 app.use("/spec", express.static(path.join(__dirname, "./spec")));
 app.use(express.static(absolutePath()));
  
-app.listen(3000);
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
